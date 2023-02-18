3158d50110fd10d96ce4723897d3b60290dc9306
nick-knudsen <45007458+nick-knudsen@users.noreply.github.com>
2023-02-18 14:08:09 -0500

    creating Grid class

--- grid.py
+++ grid.py
@@ -1,33 +1,37 @@
 # Contains encoding for crossword grid
-from tkinter import Grid
 import pandas as pd
 import numpy as np
 
-# How large to make the grid
-GRID_SIZE = 7
+class Grid:
+    def __init__(self, size):
+        # How large to make the grid
+        self.size = size
 
-# Initial probability of box being empty (will contain letter)
-P = 0.84
+        # Initial probability of box being white
+        P = 0.84
 
-#Grid Rules
-MIN_WORDS = 10
-MAX_WORDS = 30
-MIN_WORD_LENGTH = 3
+        #Grid Rules
+        MAX_WORDS = 30
+        MIN_WORD_LENGTH = 3
 
-# Contains grid shape
-# true if box is empty (will contain letter), false if box is black
-grid = np.random.choice(a=[True, False], size=(GRID_SIZE, GRID_SIZE), p=[P, 1-P])
+        # Contains grid shape
+        # true if box is white, false if box is black
+        self.grid = np.random.choice(a=[True, False], size=(self.size, self.size), p=[P, 1-P])
 
-# calculate grid stats
+    def __str__(self):
+        for box in self.grid:
+            print("O") if box else print("X")
+    
+    # calculate grid stats
 
-# counting number of words in a crossword grid:
-#   if a box above or left of a given box is black (false), current box
-#   is the first letter of a new word
+    # counting number of words in a crossword grid:
+    #   if a box above or left of a given box is black (false), current box
+    #   is the first letter of a new word
 
-# finding shortest word:
-# slice grid vertically and horizontally
-# find shortest substring of true values
+    # finding shortest word:
+    # slice grid vertically and horizontally
+    # find shortest substring of true values
 
-print(grid)
+
 
 
