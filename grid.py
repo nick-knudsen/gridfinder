66ff644f65a290788ce0004b91a80e3bee90a44c
nick-knudsen <45007458+nick-knudsen@users.noreply.github.com>
2023-02-18 17:38:36 -0500

    working on finding min word length

--- grid.py
+++ grid.py
@@ -5,7 +5,7 @@ import math
 
 class Grid:
 
-    MIN_WORD_LENGTH = 3
+    MIN_WORD_LENGTH = 2
 
 
     def __init__(self, size, p=0.84):
@@ -15,11 +15,8 @@ class Grid:
         self.max_words = math.ceil(4 + 0.312*size**2)
         # Initial probability of box being white
         self.p = p
-
         # generate random grid shape
-        # true if box is white, false if box is black
-        self.grid = np.random.choice(a=[True, False], size=(self.size, self.size), p=[p, 1-p])
-        self.enforce_symmetry()
+        self.generate_grid()
 
 
     def __str__(self):
@@ -33,6 +30,12 @@ class Grid:
                 visual += "|\n"
             return visual
 
+    def generate_grid(self):
+        # true if box is white, false if box is black
+        self.grid = np.random.choice(a=[True, False], size=(self.size, self.size), p=[self.p, 1-self.p])
+        self.enforce_symmetry()
+        self.check_validity()
+
 
     def enforce_symmetry(self):
         # enforce rotational symmetry
@@ -42,25 +45,51 @@ class Grid:
                 rotated_j = self.size - 1 - j
                 self.grid[rotated_i, rotated_j] = self.grid[i][j]
                 
-
     
     # calculate grid stats
-    def calc_stats(self):
+    def check_validity(self):
+        valid = True
         # counting number of words
-        word_count = 0
+        self.word_count = 0
         for i in range(self.size):
             for j in range(self.size):
                 if (not self.grid[i-1][j]) or (i == 0 and self.grid[i][j]):
-                    word_count += 1
+                    self.word_count += 1
                 if (not self.grid[i][j-1]) or (j == 0 and self.grid[i][j]):
-                    word_count += 1
-        print(word_count)
+                    self.word_count += 1
+
         # finding shortest word:
         # slice grid vertically and horizontally
         # find shortest substring of true values
+        for row in self.grid:
+            if not valid:
+                break
+            curr_word_length = 0
+            for box in row:
+                if box:
+                    curr_word_length += 1
+                else:
+                    if curr_word_length < self.MIN_WORD_LENGTH:
+                        # grid invalid
+                        valid = False
+                        break
+        for j in range(self.size):
+            col = self.grid[0:, j]
+            if not valid:
+                    break
+            curr_word_length = 0
+            for box in col:
+                if box:
+                    curr_word_length += 1
+                else:
+                    if curr_word_length < self.MIN_WORD_LENGTH:
+                        # grid invalid
+                        valid = False
+                        break
+        if not valid:
+            self.generate_grid()
 
-test_grid = Grid(5)
+test_grid = Grid(6)
 print(test_grid)
-test_grid.calc_stats()
 
 
